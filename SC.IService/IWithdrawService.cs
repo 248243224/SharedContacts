2813c1cba0203f75592c3c652b06def5f78a5cca
jane <10191656@kodakalaris.com>
2018-01-19 17:22:06 +0800

    update

---
+++ SC.IService/IWithdrawService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SC.Model.Entity;
+using SC.Model;
+
+namespace SC.IService
+{
+    public interface IWithdrawService
+    {
+        List<WithdrawApply> GetPagedList(JqTableParams param);
+    }
+}
