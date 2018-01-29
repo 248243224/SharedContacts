5aa4188b53db7748cf8c3ad42564e96d3a127eb1
jane <248243224@qq.com>
2018-01-29 19:38:34 +0800

    update

--- SC.WebApp/www/js/index.js
+++ SC.WebApp/www/js/index.js
@@ -28,6 +28,8 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        // disable back button in andriod
+        document.addEventListener("backbutton", this.BackButtonCallback, false);
     },
     // deviceready Event Handler
     //
@@ -40,15 +42,14 @@ var app = {
     receivedEvent: function (id) {
         if (id == "deviceready") {
             console.log('Received Event: ' + id);
-            // disable back button in andriod
-            document.addEventListener("backbutton", null, false);
             //init fast click
             FastClick.attach(document.body);
         }
     },
+    BackButtonCallback: function () { },
     RouteInit: function () {
         try {
-            angular.module('ngRouteScApp', ['ui.router','ngAnimate'])
+            angular.module('ngRouteScApp', ['ui.router', 'ngAnimate'])
                 .config(function ($stateProvider, $urlRouterProvider) {
                     $stateProvider
                         .state('guide', {
